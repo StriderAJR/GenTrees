c0ece51b66b22b7a7dab0d23635b45981e194e73
Alexander Kirsanov <alexander.a.kirsanov@gmail.com>
2019-06-17 16:16:10 +0500

    Updates

---
+++ precache-manifest.b8f2c756bd3295c9a892d85694eba423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00dd58b89d4c75a3f152ef9aed5c5385",
+    "url": "/GenTrees/index.html"
+  },
+  {
+    "revision": "bb6e1f19ac77a36f969c",
+    "url": "/GenTrees/static/css/2.13ff990d.chunk.css"
+  },
+  {
+    "revision": "214c7a7918fce243724c",
+    "url": "/GenTrees/static/css/main.18ef3d39.chunk.css"
+  },
+  {
+    "revision": "bb6e1f19ac77a36f969c",
+    "url": "/GenTrees/static/js/2.a111f749.chunk.js"
+  },
+  {
+    "revision": "214c7a7918fce243724c",
+    "url": "/GenTrees/static/js/main.89c3fad2.chunk.js"
+  },
+  {
+    "revision": "60a2a38dfe42f53f82cf",
+    "url": "/GenTrees/static/js/runtime~main.c01581eb.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/GenTrees/static/media/logo.ee7cd8ed.svg"
+  }
+]);
